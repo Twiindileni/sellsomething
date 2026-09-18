c19f7331aedf240301cb7383fd15fe1fcb7e56ea
Twiindileni Tomas <150262434+Twiindileni@users.noreply.github.com>
2026-09-18 09:49:38 +0200

    feat: add Vercel Analytics tracking

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { AuthProvider } from "./context/AuthContext";
@@ -87,6 +88,7 @@ export default function App() {
             <MobileNav />
           </div>
         </Router>
+        <Analytics />
       </ProductProvider>
     </AuthProvider>
     </HelmetProvider>
